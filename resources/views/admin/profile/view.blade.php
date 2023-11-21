2af3c73aedbeb7cc7406d2d2728ab8eb4b3fd056
taro <taro@example.com>
2023-11-21 02:44:12 +0000

    viewファイル編集

---
+++ resources/views/admin/profile/view.blade.php
@@ -0,0 +1,236 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    
+    <meta name="keywords" content="東京都,品川区,社労士,労務管理,就業規則,給与計算,助成金,障害年金,個別労使紛争">
+    <meta name="description" content="東京都品川区の渡辺社労士事務所です。人事・労務管理、就業規則作成、給与計算、助成金、障害年金等幅広く、企業様を全力でサポート致します。TEL03-3449-5201">
+    <title>渡辺社会保険労務士事務所</title>
+    
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
+    
+    <link href="{{ asset('css/original.css') }}" rel="stylesheet">
+</head>
+
+<!-- ■ ここからホームページに表示される箇所です ■ -->
+
+
+
+
+
+<!-- ここがホームページの外枠です -->
+<table width="780" cellspacing="0" cellpadding="0" style="border:0px solid blue;" bgcolor="white" align="center">
+  <tr>
+    <td>
+
+
+
+      <!-- ここがタイトルバーです -->
+      <table class="title-ber" width="100%" style="border:0px solid #cccccc; background:#ffffff background-repeat:no-repeat;" align="center">
+        <tr>
+          <td>
+            <br>
+            <h1>　渡辺社会保険労務士事務所</h1>
+            <br><br><br>
+          </td>
+        </tr>
+      </table>
+
+
+
+      <!-- ここがコンテンツバーです -->
+      <table width="100%" height="34" cellspacing="0" cellpadding="0" bgcolor="#6899d1" align="center">
+        <tr valign="middle" align="center">
+          <td width="150" class="top-bar"><span style="font-size:11pt;"><a href="{{ route('top') }}"><font color=white><strong>事務所案内</strong></font></a></span></td>
+          <td width="150" class="top-bar"><span style="font-size:11pt;"><a href="{{ route('kaze') }}"><font color=white><strong>風のたより</strong></font></a></span></td>
+          <td width="150" class="top-bar"><span style="font-size:11pt;"><a href="{{ route('new') }}"><font color=white><strong>新着情報</strong></font></a></span></td>
+          <td width="150" class="top-bar"><span style="font-size:11pt;"><a href="{{ route('form') }}"><font color=white><strong>お問い合せ</strong></font></a></span></td>
+         </tr>
+      </table>
+
+
+
+      <!-- ここがサイドバー ＋ メインコンテンツです -->
+      <table border="0" width="100%" cellpadding="2" cellspacing="0" align="center">
+        <tr align="left" valign="top">
+          <td>
+
+            <!-- ここがサイドバーです -->
+            <!--<iframe height="600" width="100%" frameborder="0" marginheight="0" marginwidth="0" scrolling="no"></iframe>-->
+            <!-- ここがホームページ（サイドバー）の外枠です -->
+            <table border="0" width="100%" cellpadding="0" cellspacing="0" bgcolor="#ffffff" align="center">
+              <tr align="center" valign="top">
+                <td>
+
+
+
+      <!-- ここがサイドバーです -->
+                <div style="line-height:40%;">&nbsp;</div>
+                  <table width="100%" cellpadding="0" cellspacing="0" style="border:1px solid #e1e1e1; padding:0px; background-color:#ffffff;">
+                    <tr align="left" valign="middle" height="42">
+                      <td class="side-bar-bg">
+                        <div style="font-size:11pt;">　<img src="{{ asset('/image/c6.gif') }}" alt="">　<a href="{{ route('top') }}"><strong>事務所案内</strong></a></div>
+                      </td>
+                    </tr>
+                    <tr align="left" valign="middle" height="42">
+                      <td class="side-bar-bg">
+                        <div style="font-size:11pt;">　<img src="{{ asset('/image/c6.gif') }}" alt="">　<a href="{{ route('top') }}"><strong>サービス案内</strong></a></div>
+                      </td>
+                    </tr>
+                    <tr align="left" valign="middle" height="42">
+                      <td class="side-bar-bg">
+                        <div style="font-size:11pt;">　<img src="{{ asset('/image/c6.gif') }}" alt="">　<a href="{{ route('kaze') }}"><strong>風のたより</strong></a></div>
+                      </td>
+                    </tr>
+                    <tr align="left" valign="middle" height="42">
+                      <td class="side-bar-bg">
+                        <div style="font-size:11pt;">　<img src="{{ asset('/image/c6.gif') }}" alt="">　<a href="{{ route('new') }}"><strong>新着情報</strong></a></div>
+                      </td>
+                    </tr>
+                    <tr align="left" valign="middle" height="42">
+                      <td class="side-bar-bg">
+                        <div style="font-size:11pt;">　<img src="{{ asset('/image/c6.gif') }}" alt="">　<a href="{{ route('form') }}"><strong>お問い合せ</strong></a></div>
+                      </td>
+                    </tr>
+                    <tr align="left" valign="middle" height="42">
+                      <td class="side-bar-bg">
+                        <div style="font-size:11pt;">　<img src="{{ asset('/image/c6.gif') }}" alt="">　<a href="{{ route('top') }}"><strong>プライバシーポリシー</strong></a></div>
+                      </td>
+                    </tr>
+                  </table>
+                <div style="line-height:70%;">&nbsp;</div>
+
+
+
+      <!-- ここがお気に入りバーです -->
+      <button onclick="window.external.addFavorite('','渡辺社労士事務所');" name="お気に入りに追加する" style="height:16pt;text-align:center; margin-top:0pt; line-height:100%; width:155px;">お気に入りに追加する</button>
+      <div style="line-height:70%;">&nbsp;</div>
+
+
+
+      <!-- ここがその他の情報です -->
+      <table class="others" border="0" width="100%" height="32" cellspacing="0" cellpadding="0" style="background:#0069A3;" align="center">
+        <tr align="center" valign="middle">
+          <td>
+            <font color="white"><span style="font-size:11pt;"><strong>お問い合せ</strong></span></font>
+          </td>
+        </tr>
+      </table>
+      <table width="100%" cellpadding="0" cellspacing="0" style="border:1px solid green; background-color:#eeeeee;" align="center">
+        <tr align="left" valign="middle" height="46">
+          <td style="padding:8px">
+          　渡辺社労士事務所<br>
+          　〒141-0022<br>
+          　東京都品川区<br>
+          　　　東五反田3-2-6<br>
+          　TEL:03-3449-5201<br>
+          　FAX:03-3449-5257<br>
+          </td>
+        </tr>
+      </table>
+      <div style="line-height:70%;">&nbsp;</div>
+
+
+
+    </td>
+  </tr>
+</table><br>
+
+          </td>
+          <td>
+
+
+
+            <!-- ここがメインコンテンツです -->
+            <table border="0" width="580" cellpadding="0" cellspacing="0">
+              <tr valign="top">
+                <td>
+
+
+
+                  <!-- ここが「コンテンツ１」を掲載している箇所です -->
+                  <div style="line-height:40%;">&nbsp;</div>
+                  <table width="99%" height="29" cellspacing="0" cellpadding="0" style="border:1px solid #cccccc; background:#ffffff url(bar6.gif);" align="center">
+                    <tr align="left" valign="middle">
+                      <td>
+                        <h2>　事務所案内</h2>
+                      </td>
+                    </tr>
+                  </table>
+                  <table width="99%" style="border:1px solid #c7c7c7" bgcolor="#ffffff" cellspacing="0" align="center">
+                    <tr>
+                      <td align="left" valign="top" style="padding:13px">
+                        <h4>ご挨拶</h4>
+                        渡辺社会保険労務士事務所代表の社会保険労務士 渡辺 眞一郎と申します。<br><br>
+                        当事務所は、お客様から信頼を頂けるパートナーであることを目指し<br>
+                        東京都品川区で、開業しております。<br>
+                        御社の様々な悩みに対して全力でサポートさせていただきます。<br>
+                        お気軽にお問い合わせください。<br><br>
+
+                  <!-- ここが「コンテンツ１」を掲載している箇所です -->
+              <!--    <div style="line-height:40%;">&nbsp;</div>
+                  <table width="99%" height="29" cellspacing="0" cellpadding="0" style="border:1px solid #cccccc; background:#ffffff url(bar6.gif);" align="center">-->
+                   <tr align="left" valign="middle">
+                      <td>
+                        <h4>　事務所概要</h4>
+                      </td>
+                    </tr>
+                  </table>
+                  <table width="99%" style="border:1px solid #c7c7c7" bgcolor="#ffffff" cellspacing="0" align="center">
+                    <tr>
+                      <td align="left" valign="top" style="padding:13px">
+                       <div class="ptxt"><p><strong><font color="#999999">事務所名</font>　　</strong>渡辺社会保険労務士事務所<hr id="null"><p></p><font size="3"></font>
+                                         <p><strong><font color="#999999">代表者名</font>　　</strong>特定社会保険労務士　渡邉　眞一郎<hr id="null"><p></p>
+                                         <p><strong><font color="#999999">住　所</font>　　　</strong>〒141-0022　東京都品川区東五反田3-2-6<hr id="null"><p></p>
+                                         <p><strong><font color="#999999">電話番号</font>　　</strong>03-3449-5201<hr id="null"><p></p>
+                                         <p><strong><font color="#999999">Fax番号</font>　　</strong> 03-3449-5257<hr id="null"><p></p><!--END:ptxt-->
+                                         <p><strong><font color="#999999">E-mail</font>　　</strong> watanabe-4444@muc.biglobe.ne.jp<hr id="null"><p></p><!--END:ptxt-->
+                                
+                        </div><br>
+                       <h4>事務所地図</h4>
+                       <iframe class="gm" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3242.8774512119658!2d139.72760167623636!3d35.63074753274484!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60188aff3040ca17%3A0x7bd915b7a41319a8!2z44CSMTQxLTAwMjIg5p2x5Lqs6YO95ZOB5bed5Yy65p2x5LqU5Y-N55Sw77yT5LiB55uu77yS4oiS77yW!5e0!3m2!1sja!2sjp!4v1699345286049!5m2!1sja!2sjp" allowfullscreen frameborder="0" scrolling="auto" width="100%" height="450"></iframe>
+
+<br><br>
+                      </td>
+                    </tr>
+                  </table>
+
+
+
+                </td>
+              </tr>
+            </table>
+
+
+
+          </td>
+        </tr>
+      </table>
+
+
+      <!-- pagetop -->
+      <p class="pagetop"><a href="#top"><img src="{{ asset('/image/totop.gif') }}" width="78" heihtt="20" alt="ページ先頭へ" /></a></p>
+
+
+      <!-- 一番下に掲載しているフッタです -->
+      <table width="100%" cellpadding="0" cellspacing="0" align="center">
+        <tr align="center" valign="middle" height="34">
+          <td background="under-bar6.jpg">
+            <font color="white"><strong>Copyright &copy; 2019 <a href="index.html"><font color=white>渡邉社会保険労務士事務所</font></a> All rights reserved.</strong></font>
+          </td>
+        </tr>
+      </table>
+
+    </td>
+  </tr>
+</table>
+
+
+
+</body>
+</html>
